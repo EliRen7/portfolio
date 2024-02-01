14f2d4c8ff4cda078239e8e315d029998f206c2e
Elizabeth Renderos <elirenderos@gmail.com>
2024-02-01 11:54:26 +0900

    updated resume

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -10,7 +10,6 @@ function Contact() {
     <div id='contact'>
       <h5>Get in Touch!</h5>
       <h2>Contact Me</h2>
-
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
